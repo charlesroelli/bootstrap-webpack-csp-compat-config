592de0ea014bf9554dfbaf415cf45489a52814f9
scallywag <scallywag@users.noreply.github.com>
2022-05-09 21:04:54 +0200

    Set up mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
+    plugins: [new MiniCssExtractPlugin()],
     module: {
 	rules: [
-	    { test: /\.css$/, use: 'css-loader' },
+	    {
+		test: /\.css$/,
+		use: [
+		    MiniCssExtractPlugin.loader,
+		    'css-loader'
+		]},
 	    {
 		mimetype: 'image/svg+xml',
 		scheme: 'data',
