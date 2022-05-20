8e387f90a61ce1daf6dc7e45bb45fea0a245045e
scallywag <scallywag@users.noreply.github.com>
2022-05-20 21:33:53 +0200

    Reformat webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-	rules: [
-	    {
-		test: /\.scss$/,
-		use: [
-		    MiniCssExtractPlugin.loader,
-		    'css-loader',
-		    {
-			loader: 'postcss-loader',
-			options: {
-			    postcssOptions: {
-				plugins: function () {
-				    return [
-					require('autoprefixer')
-				    ];
+	plugins: [new MiniCssExtractPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: function () {
+									return [
+										require('autoprefixer')
+									];
+								}
+							}
+						}
+					},
+					'sass-loader'
+				]
+			},
+			{
+				mimetype: 'image/svg+xml',
+				scheme: 'data',
+				type: 'asset/resource',
+				generator: {
+					filename: 'icons/[hash].svg'
 				}
-			    }
 			}
-		    },
-		    'sass-loader'
-		]},
-	    {
-		mimetype: 'image/svg+xml',
-		scheme: 'data',
-		type: 'asset/resource',
-		generator: {
-		    filename: 'icons/[hash].svg'
-		}
-	    }
-	]
-    }
+		]
+	}
 };
